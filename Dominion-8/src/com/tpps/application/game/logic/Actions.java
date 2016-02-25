94d0ae19a90de8ccd6b3de29c6672bd1645727b3
Lukas Karl Adler <ladler@mail.uni-mannheim.de>
2016-02-25 18:12:48 +0100

    [DOM8-15] create Class CardObject

--- Dominion-8/src/com/tpps/application/game/logic/Actions.java
+++ Dominion-8/src/com/tpps/application/game/logic/Actions.java
@@ -1,5 +1,9 @@
 package com.tpps.application.game.logic;
-
+/**
+ * 
+ * @author ladler - Lukas Adler
+ *
+ */
 public enum Actions {
 	 DRAWCARDS, TRASH, DISCARD, ADDACTIONTOPLAYER, ADDTEMPORARYMONEYFORTURN, ADDPURCHASE;
-}
+	}
