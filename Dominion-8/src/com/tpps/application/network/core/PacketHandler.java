a86dc2d14db66f3e54d405e317f5d959b3f4913c
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2016-02-25 19:12:43 +0100

    [DOM8-24] fixed repository-inconsistency

---
+++ Dominion-8/src/com/tpps/application/network/core/PacketHandler.java
@@ -0,0 +1,42 @@
+package com.tpps.application.network.core;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public abstract class PacketHandler {
+	
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+	// represents the server-instance who's packets are handled by this PacketHandler-instance
+	protected Server parent;
+	
+	public abstract void handleReceivedPacket(int port, byte[] bytes);
+	
+
+	/**
+	 * outputs a String to the console
+	 * 
+	 * @author sjacobs - Steffen Jacobs
+	 */
+	// TODO: save log
+	public void output(String str) {
+		System.out.println(sdf.format(new Date()) + ": " + str);
+	}
+
+	/**
+	 * constructor with parent server-instance
+	 * 
+	 * @author sjacobs - Steffen Jacobs
+	 */
+	public PacketHandler(Server _parent){
+		this.parent = _parent;
+	}
+	
+	public PacketHandler(){
+		
+	}
+	
+	public void setParent(Server _parent){
+		this.parent =_parent;
+	}
+}
