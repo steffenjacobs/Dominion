b896135b94861a97fc4c3e53e9d0ab7ae6fa8e0d
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2016-02-25 19:11:11 +0100

    [DOM8-27] added Registration-Packets

---
+++ Dominion-8/src/com/tpps/application/network/login/packets/PacketRegisterRequest.java
@@ -0,0 +1,60 @@
+package com.tpps.application.network.login.packets;
+
+import com.tpps.application.network.packet.Packet;
+import com.tpps.application.network.packet.PacketType;
+
+/**
+ * This packet is send as a request from a game client to the LoginServer to
+ * check a users credentials.
+ * 
+ * @author sjacobs - Steffen Jacobs
+ */
+public class PacketRegisterRequest extends Packet {
+	private static final long serialVersionUID = -2467347347400740313L;
+	private final String username, hashedPW, emailAddress;
+
+	/**
+	 * @return the username
+	 * @author sjacobs - Steffen Jacobs
+	 */
+	public String getUsername() {
+		return this.username;
+	}
+
+	/**
+	 * @return the hashed password
+	 * @author sjacobs - Steffen Jacobs
+	 */
+	public String getHashedPW() {
+		return this.hashedPW;
+	}
+	
+	/**
+	 * @return the email address of the user who registers
+	 * @author sjacobs - Steffen Jacobs
+	 */
+	public String getEmail() {
+		return this.emailAddress;
+	}
+
+	/**
+	 * initializes the Packet with a username and a hashed password to check
+	 * 
+	 * @author sjacobs - Steffen Jacobs
+	 */
+	public PacketRegisterRequest(String name, String _hashedPW, String _emailAddress) {
+		super(PacketType.SESSION_CHECK_REQUEST);
+		this.username = name;
+		this.hashedPW = _hashedPW;
+		this.emailAddress = _emailAddress;
+	}
+
+	/**
+	 * @return a readable representation of the packet
+	 * @author sjacobs - Steffen Jacobs
+	 */
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName() + ": " + this.getUsername() + " / " + this.emailAddress + " - " + this.getHashedPW();
+	}
+}
