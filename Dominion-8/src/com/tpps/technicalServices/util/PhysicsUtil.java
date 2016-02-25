6dba0f3e0e851785ea23a1e2cf2e987775afda7f
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2016-02-25 22:03:27 +0100

    [DOM8-30] added MathUtil with signum-method and PhysicsUtil with collision-method

---
+++ Dominion-8/src/com/tpps/technicalServices/util/PhysicsUtil.java
@@ -0,0 +1,20 @@
+package com.tpps.technicalServices.util;
+
+import java.awt.Rectangle;
+
+/**
+ * provides some useful physic-calculations
+ * 
+ * @author sjacobs
+ */
+public final class PhysicsUtil {
+	/**
+	 * checks wheter two 2D rectangular areas overlap
+	 * 
+	 * @author sjacobs
+	 */
+	public static boolean collides(Rectangle area1, Rectangle area2) {
+		return area1.x < area2.x + area2.width && area1.x + area1.width > area2.x && area1.y < area2.y + area2.height
+				&& area1.height + area1.y > area2.y;
+	}
+}
