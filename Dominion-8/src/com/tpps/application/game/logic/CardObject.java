3eaaa60ffe0dee88b99d25ada1fda8f9bcf86114
Nicolas Christian Wipfler <nwipfler@mail.uni-mannheim.de>
2016-02-26 02:11:35 +0100

    [DOM8-11] added 'types' attribute to CardObject class

--- Dominion-8/src/com/tpps/application/game/logic/CardObject.java
+++ Dominion-8/src/com/tpps/application/game/logic/CardObject.java
@@ -12,19 +12,22 @@ import com.tpps.ui.GraphicFramework;
  */
 
 public class CardObject extends GameObject {
-	
+
 	private static final long serialVersionUID = 1L;
 	private final Actions[] actions;
-	private final int costs;
+	private final Types[] types;
+	private final int cost;
 
 	/**
-	 * sets the actions array containing the actions which the cardObject will execute
+	 * sets the actions array containing the actions which the cardObject will
+	 * execute
 	 */
-	public CardObject(Actions[] actions, int costs, int locX, int locY,
-			int _layer, Image sourceImage, GraphicFramework _parent) {
-		super(locX, locY, _layer, sourceImage,_parent);
+	public CardObject(int costs, int locX, int locY, int _layer, Image sourceImage, GraphicFramework _parent,
+			Actions[] actions, Types[] types) {
+		super(locX, locY, _layer, sourceImage, _parent);
 		this.actions = actions;
-		this.costs = costs;
+		this.cost = costs;
+		this.types = types;
 	}
 
 	/**
@@ -36,28 +39,27 @@ public class CardObject extends GameObject {
 		for (int i = 0; i < actions.length; i++) {
 
 			switch (actions[i]) {
-			case ADD_ACTION_TO_PLAYER:
-				// call
-				break;
-			case ADD_PURCHASE:
-				// call
-				break;
 			case DRAW_CARDS:
 				// call
 				break;
 			case DISCARD:
 				// call
 				break;
-			case ADD_TEMPORARY_MONEY_FOR_TURN:
-				// call
-				break;
 			case TRASH:
 				// call
 				break;
 			case GAIN:
-//				call
+				// call
 				break;
-
+			case ADD_ACTION_TO_PLAYER:
+				// call
+				break;
+			case ADD_PURCHASE:
+				// call
+				break;
+			case ADD_TEMPORARY_MONEY_FOR_TURN:
+				// call
+				break;
 			}
 		}
 	}
@@ -71,24 +73,24 @@ public class CardObject extends GameObject {
 	@Override
 	public void onMouseEnter() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onMouseExit() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onMouseClick() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onMouseDrag() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
