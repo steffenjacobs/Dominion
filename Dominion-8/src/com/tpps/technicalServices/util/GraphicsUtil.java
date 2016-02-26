217cf26e8470640c037c92e5882c02f7b3342901
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2016-02-26 18:27:58 +0100

    [DOM8-10] added methods to draw text centered on top of image

--- Dominion-8/src/com/tpps/technicalServices/util/GraphicsUtil.java
+++ Dominion-8/src/com/tpps/technicalServices/util/GraphicsUtil.java
@@ -1,16 +1,23 @@
 package com.tpps.technicalServices.util;
 
 import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 
-/** provides some useful methods for manipulating graphic objects
- * @author sjacobs - Steffen Jacobs */
+/**
+ * provides some useful methods for manipulating graphic objects
+ * 
+ * @author sjacobs - Steffen Jacobs
+ */
 public final class GraphicsUtil {
 
 	/**
-	 * @author sjacobs - Steffen Jacobs sets the alpha-value for an image
+	 * @author sjacobs, jhuhn sets the alpha-value for an image synchronously
 	 * @param transparency
 	 *            new Alpha-Value for desired image
 	 * @param img
@@ -21,13 +28,60 @@ public final class GraphicsUtil {
 		BufferedImage tmpImg = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2d = (Graphics2D) tmpImg.getGraphics();
 		g2d.setComposite(AlphaComposite.SrcOver.derive(transparency));
-		// set the transparency level in range 0.0f - 1.0f
-		// g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,
-		// transparency));
-		// AlphaComposite ac =
-		// java.awt.AlphaComposite.getInstance(AlphaComposite.SRC_OVER,
-		// transparency);
 		g2d.drawImage(img, 0, 0, null);
 		return tmpImg;
+	}
+
+	/**
+	 * draws a String centered on top of an image
+	 * 
+	 * @param img:
+	 *            Image to draw on top of
+	 * @param caption:
+	 *            text to draw
+	 * @param font:
+	 *            font to draw text with
+	 * @param fontColor:
+	 *            color of the font
+	 * @param offsetX:
+	 *            offset left and right
+	 * @param offsetY:
+	 *            offset up and down
+	 * @return a new rendered image with the caption on top of the old image
+	 * @author sjacobs - Steffen Jacobs
+	 */
+	public static Image drawStringCentered(Image img, String caption, Font font, Color fontColor, int offsetX,
+			int offsetY) {
+		BufferedImage rendered = new BufferedImage(img.getWidth(null), img.getHeight(null),
+				BufferedImage.TYPE_INT_ARGB);
+		Graphics2D graphics = (Graphics2D) rendered.getGraphics();
+		graphics.drawImage(img, 0, 0, null);
+		graphics.setFont(font);
+		graphics.setColor(fontColor);
+
+		FontMetrics fm = graphics.getFontMetrics();
+		Rectangle2D r = fm.getStringBounds(caption, graphics);
+		int x = (img.getWidth(null) - (int) r.getWidth()) / 2 + offsetX;
+		int y = (img.getHeight(null) - (int) r.getHeight()) / 2 + fm.getAscent() + offsetY;
+		graphics.drawString(caption, x, y);
+		return rendered;
+	}
+
+	/**
+	 * draws a String centered on top of an image
+	 * 
+	 * @param img:
+	 *            Image to draw on top of
+	 * @param caption:
+	 *            text to draw
+	 * @param font:
+	 *            font to draw text with
+	 * @param fontColor:
+	 *            color of the font
+	 * @return a new rendered image with the caption on top of the old image
+	 * @author sjacobs - Steffen Jacobs
+	 */
+	public static Image drawStringCentered(Image img, String caption, Font font, Color fontColor) {
+		return drawStringCentered(img, caption, font, fontColor, 0, 0);
 	}
 }
