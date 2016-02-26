1d4a6a0c2447389076f5f2e78ce93bb828d4b4c5
Nicolas Christian Wipfler <nwipfler@mail.uni-mannheim.de>
2016-02-26 01:38:25 +0100

    [DOM8-14] Created Card Type enum.

---
+++ Dominion-8/src/com/tpps/application/game/logic/Types.java
@@ -0,0 +1,10 @@
+package com.tpps.application.game.logic;
+
+/**
+ * 
+ * @author nwipfler - Nicolas Wipfler
+ *
+ */
+public enum Types {
+	ACTION, REACTION, ATTACK, VICTORY, CURSE, TREASURE, DURATION, PRIZE, RUINS, SHELTER, LOOTER, KNIGHT, TRAVELLER, RESERVE;
+}
