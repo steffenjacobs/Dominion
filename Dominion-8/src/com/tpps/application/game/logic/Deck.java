c9c4991841048e5afa0b0942809a9b3fdd2498aa
Nicolas Christian Wipfler <nwipfler@mail.uni-mannheim.de>
2016-02-27 15:21:29 +0100

    [DOM8-39] commented Deck class with design plan

--- Dominion-8/src/com/tpps/application/game/logic/Deck.java
+++ Dominion-8/src/com/tpps/application/game/logic/Deck.java
@@ -2,5 +2,11 @@ package com.tpps.application.game.logic;
 
 public class Deck {
 
-	// TODO: implement Deck as a Stack
+	// TODO: implement Deck as a Collection
+	// 
+	// Deck has two ArrayLists with CardObjects: drawPile and discardPile
+	// every Player has a Deck
+	// Deck provides functionality to handle the players cards, shuffle, redraw before turn, manage discarded cards etc.
+
+	
 }
