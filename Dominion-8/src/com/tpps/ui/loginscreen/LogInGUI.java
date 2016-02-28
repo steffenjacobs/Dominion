ebb72effd0ad1440cb620eda8105ed84b1671ee7
Nishit Agrawal <nagrawal@mail.uni-mannheim.de>
2016-02-28 01:45:34 +0100

    [DOM8-41] - Path fix

--- Dominion-8/src/com/tpps/ui/loginscreen/LogInGUI.java
+++ Dominion-8/src/com/tpps/ui/loginscreen/LogInGUI.java
@@ -1,4 +1,5 @@
 package com.tpps.ui.loginscreen;
+
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
@@ -11,6 +12,7 @@ import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -25,7 +27,7 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 //TODO: underline Header, set Background, set transparent, map 'RETURN' key to Login
 public class LogInGUI extends JFrame {
-	
+
 	private static final long serialVersionUID = 1L;
 	private int width;
 	private int height;
@@ -42,25 +44,26 @@ public class LogInGUI extends JFrame {
 	private JLabel header;
 	private JLabel all;
 	private JPanel[] panels;
-	private Font smallfont,customFont;
-	
-	public LogInGUI(){	
+	private Font smallfont, customFont;
+
+	public LogInGUI() {
 		createdComponent();
 	}
+
 	public LogInGUI(String text, char[] password) {
 		createdComponent();
 		this.userinfo.setText(text);
 		this.passwordbox.setText(String.valueOf(password));
 	}
-	
-	private void createdComponent(){
+
+	private void createdComponent() {
 		width = Toolkit.getDefaultToolkit().getScreenSize().width;
 		height = Toolkit.getDefaultToolkit().getScreenSize().height;
-		
+
 		loadImage();
 		resizeImage();
 		importFont();
-		
+
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (ClassNotFoundException e) {
@@ -79,93 +82,90 @@ public class LogInGUI extends JFrame {
 		this.createpanel4();
 		this.revalidate();
 	}
-	
-	
-	private void init(){
+
+	private void init() {
 		c = this.getContentPane();
 		all = new JLabel(loading);
-		all.setLayout(new GridLayout(4, 1,0,30));
-		this.setSize(width/4, height/4);
+		all.setLayout(new GridLayout(4, 1, 0, 30));
+		this.setSize(width / 4, height / 4);
 		this.setLocationRelativeTo(null);
 		this.setTitle("Log in !");
 		this.setResizable(false);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		smallfont = new Font("Calibri", Font.BOLD, 19);
-		
+
 		description = new JLabel[2];
 		panels = new JPanel[4];
 		for (int i = 0; i < panels.length; i++) {
-			panels[i] = new JPanel(new FlowLayout());			
+			panels[i] = new JPanel(new FlowLayout());
 		}
-//		this.setContentPane(new JLabel(loading));
+		// this.setContentPane(new JLabel(loading));
 	}
-	
-	//not working why? Path checking!!
-	private void importFont(){
+
+	// not working why? Path checking!!
+	private void importFont() {
+
 		try {
-			customFont = Font.createFont(Font.TRUETYPE_FONT, new File( System.getProperty("user.dir"), "src/resources/font/xenippa1.TTF"));
-		     GraphicsEnvironment ge = 
-		         GraphicsEnvironment.getLocalGraphicsEnvironment();
-//		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("src/resources/font/xenippa1.TTF")));
-		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File( System.getProperty("user.dir"), "src/resources/font/xenippa1.TTF")));
+			customFont = Font.createFont(Font.TRUETYPE_FONT,
+					new File(URLDecoder.decode(ClassLoader.getSystemResource("resources/font/xenippa1.TTF").getFile(),"UTF-8")));
+			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT,
+					new File(URLDecoder.decode(ClassLoader.getSystemResource("resources/font/xenippa1.TTF").getFile(),"UTF-8"))));
 		} catch (Exception e) {
 			System.err.println(e);
 		}
 	}
-	
+
 	private void loadImage() {
 		try {
-			this.background = ImageIO.read(ClassLoader
-					.getSystemResource("resources/img/loginScreen/LoginBackground.jpg"));
+			this.background = ImageIO
+					.read(ClassLoader.getSystemResource("resources/img/loginScreen/LoginBackground.jpg"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
 
-	private void resizeImage(){
-		loading= new ImageIcon(background);
-		Image newing = background.getScaledInstance(width/4, height/4, java.awt.Image.SCALE_SMOOTH);
+	private void resizeImage() {
+		loading = new ImageIcon(background);
+		Image newing = background.getScaledInstance(width / 4, height / 4, java.awt.Image.SCALE_SMOOTH);
 		loading = new ImageIcon(newing);
 	}
 
-
-	
-	private void createpanel1(){
+	private void createpanel1() {
 		header = new JLabel();
-		header.setText("Type in Userinformation");	
+		header.setText("Type in Userinformation");
 		header.setFont(customFont.deriveFont(25f));
 		header.setForeground(Color.WHITE);
-		
+
 		panels[0].add(header);
 		panels[0].setOpaque(false);
-	//	panels[0].setBorder(BorderFactory.createLineBorder(Color.GREEN, 4));
+		// panels[0].setBorder(BorderFactory.createLineBorder(Color.GREEN, 4));
 		all.add(panels[0]);
-//		c.add(panels[0]);
+		// c.add(panels[0]);
 	}
-	
-	private void createpanel2(){
+
+	private void createpanel2() {
 		panels[1].setLayout(new GridLayout(1, 2));
-		
+
 		description[0] = new JLabel("Accountname: ");
 		description[0].setFont(smallfont);
 		description[0].setHorizontalAlignment(JLabel.CENTER);
 		userinfo = new JTextField();
-		
+
 		userinfo.setOpaque(false);
 		userinfo.setFont(smallfont);
 		panels[1].add(description[0]);
 		panels[1].add(userinfo);
 		panels[1].setOpaque(false);
-	//	panels[1].setBorder(BorderFactory.createLineBorder(Color.RED, 4));
+		// panels[1].setBorder(BorderFactory.createLineBorder(Color.RED, 4));
 		all.add(panels[1]);
 		panels[0].revalidate();
 	}
-	
-	private void createpanel3(){
+
+	private void createpanel3() {
 		panels[2].setLayout(new GridLayout(1, 2));
-		
+
 		description[1] = new JLabel("Password: ");
 		description[1].setFont(smallfont);
 		description[1].setHorizontalAlignment(JLabel.CENTER);
@@ -174,24 +174,24 @@ public class LogInGUI extends JFrame {
 		panels[2].add(description[1]);
 		panels[2].add(passwordbox);
 		panels[2].setOpaque(false);
-	//	panels[2].setBorder(BorderFactory.createLineBorder(Color.CYAN, 4));
+		// panels[2].setBorder(BorderFactory.createLineBorder(Color.CYAN, 4));
 		all.add(panels[2]);
-		panels[2].revalidate();		
+		panels[2].revalidate();
 	}
-	
-	private void createpanel4(){
+
+	private void createpanel4() {
 		panels[3].setLayout(new FlowLayout(FlowLayout.CENTER, 20, 0));
-	//	panels[3].setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
-		
+		// panels[3].setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+
 		execute = new JButton("Login");
 		execute.setFont(customFont.deriveFont(15f));
-		execute.setPreferredSize(new Dimension(120,30));
+		execute.setPreferredSize(new Dimension(120, 30));
 		cancel = new JButton("Cancel");
 		cancel.setFont(customFont.deriveFont(15f));
 		createAccount = new JButton("New Account");
 		createAccount.setFont(customFont.deriveFont(10f));
-		cancel.setPreferredSize(new Dimension(120,30));
-		createAccount.setPreferredSize(new Dimension(120,30));
+		cancel.setPreferredSize(new Dimension(120, 30));
+		createAccount.setPreferredSize(new Dimension(120, 30));
 		panels[3].add(execute);
 		panels[3].add(createAccount);
 		panels[3].add(cancel);
@@ -199,13 +199,13 @@ public class LogInGUI extends JFrame {
 		all.add(panels[3]);
 		c.add(all);
 		panels[3].revalidate();
-		
-		createAccount.addActionListener(new LoginListener(createAccount,this));
-		cancel.addActionListener(new LoginListener(cancel,this));
-		execute.addActionListener(new LoginListener(execute,this));
+
+		createAccount.addActionListener(new LoginListener(createAccount, this));
+		cancel.addActionListener(new LoginListener(cancel, this));
+		execute.addActionListener(new LoginListener(execute, this));
 	}
-	
-	public static void main(String[] args)  {		
+
+	public static void main(String[] args) {
 		new LogInGUI();
 	}
 }
