df21ce019a421c5168089b3a3c19dce9c33a8306
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2016-03-01 01:00:01 +0100

    [DOM8-24] added 2 packets for gameSessions

---
+++ Dominion-8/src/com/tpps/application/network/gameSession/packets/PacketEndTurn.java
@@ -0,0 +1,43 @@
+package com.tpps.application.network.gameSession.packets;
+
+import com.tpps.application.network.packet.Packet;
+import com.tpps.application.network.packet.PacketType;
+
+/**
+ * This packet is send from a client to the server every time the player wants
+ * to end his turn. The Server then distributes the packet to the other players.
+ * 
+ * @author sjacobs - Steffen Jacobs
+ */
+public class PacketEndTurn extends Packet {
+	private static final long serialVersionUID = -8763060018038893429L;
+	private final String username;
+
+	/**
+	 * @return the name of the player who played wants to end the turn
+	 * @author sjacobs - Steffen Jacobs
+	 */
+	public String getUsername() {
+		return this.username;
+	}
+
+	/**
+	 * initializes the Packet with the name of the player who wants to end the
+	 * turn
+	 * 
+	 * @author sjacobs - Steffen Jacobs
+	 */
+	public PacketEndTurn(String playerName) {
+		super(PacketType.END_TURN);
+		this.username = playerName;
+	}
+
+	/**
+	 * @return a readable String
+	 * @author sjacobs - Steffen Jacobs
+	 */
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName() + ": " + this.getUsername();
+	}
+}
---
+++ Dominion-8/src/com/tpps/application/network/gameSession/packets/PacketPlayCard.java
@@ -0,0 +1,53 @@
+package com.tpps.application.network.gameSession.packets;
+
+import com.tpps.application.network.packet.Packet;
+import com.tpps.application.network.packet.PacketType;
+
+/**
+ * This packet is send from the client to the server everytime the client draws
+ * a card. The Server then distributes the packet to the other clients.
+ * 
+ * @author sjacobs - Steffen Jacobs
+ */
+public class PacketPlayCard extends Packet {
+	private static final long serialVersionUID = -8763060018038893429L;
+	private final int playedCardID;
+	private final String username;
+
+	/**
+	 * @return the name of the player who played the card
+	 * @author sjacobs - Steffen Jacobs
+	 */
+	public String getUsername() {
+		return this.username;
+	}
+
+	/**
+	 * @return the playedCard
+	 * @author sjacobs - Steffen Jacobs
+	 */
+	public int getCardID() {
+		return this.playedCardID;
+	}
+
+	/**
+	 * initializes the Packet with the id of the card which had been played and
+	 * the name of the player who played the card
+	 * 
+	 * @author sjacobs - Steffen Jacobs
+	 */
+	public PacketPlayCard(int cardID, String playerName) {
+		super(PacketType.CARD_PLAYED);
+		this.playedCardID = cardID;
+		this.username = playerName;
+	}
+
+	/**
+	 * @return a readable String
+	 * @author sjacobs - Steffen Jacobs
+	 */
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName() + ": " + this.getUsername() + " - " + this.getCardID();
+	}
+}
