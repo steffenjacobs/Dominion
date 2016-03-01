96262d75c7d32e4b391e3be42dd4ab31648885e7
Nicolas Christian Wipfler <nwipfler@mail.uni-mannheim.de>
2016-03-01 22:56:17 +0100

    [DOM8-41] some formatting

--- Dominion-8/src/com/tpps/application/game/card/CardAction.java
+++ Dominion-8/src/com/tpps/application/game/card/CardAction.java
@@ -7,45 +7,7 @@ package com.tpps.application.game.card;
  *
  */
 public enum CardAction {
-	// @Lukas, keine Ahnung ob der auskommentierte Code so viel Sinn macht, habs mal vorläufig geändert
-	
+
 	COUNT_FOR_VICTORY, ADD_ACTION_TO_PLAYER, ADD_TEMPORARY_MONEY_FOR_TURN, ADD_PURCHASE, DRAW, PUT_BACK, GAIN, DISCARD, TRASH, REVEAL, NONE;
 
-	// COUNT_FOR_VICTORY(-1), ADD_ACTION_TO_PLAYER(-1),
-	// ADD_TEMPORARY_MONEY_FOR_TURN(-1), ADD_PURCHASE(-1), DRAW(-1),
-	// PUT_BACK(null),
-	// GAIN(-1), DISCARD(-1), TRASH(null), REVEAL(-1), NONE(0) /* ab hier DLC:
-	// CHOOSE, PASS, NAME, COST_LESS */;
-	//
-	// private int amount;
-	// private Type type;
-	//
-	// private Action(int amount) {
-	// this.amount = amount;
-	// }
-	//
-	// private Action(Type type) {
-	// this.type = type;
-	// }
-	//
-	// private Action(int amount, Type type) {
-	// this.amount = amount;
-	// this.type = type;
-	// }
-	//
-	// public Type getType() {
-	// return type;
-	// }
-	//
-	// public void setType(Type type) {
-	// this.type = type;
-	// }
-	//
-	// public int getAmount() {
-	// return this.amount;
-	// }
-	//
-	// public void setAmount(int amount) {
-	// this.amount = amount;
-	// }
 }
