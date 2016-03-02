7df2e3939b092b653250281471285c44d05aac59
Lukas Karl Adler <ladler@mail.uni-mannheim.de>
2016-03-02 16:32:03 +0100

    [DOM8-11] move doAction()

--- Dominion-8/src/com/tpps/application/game/ServerPlayer.java
+++ Dominion-8/src/com/tpps/application/game/ServerPlayer.java
@@ -1,5 +1,9 @@
 package com.tpps.application.game;
 
+import java.util.ArrayList;
+
+import com.tpps.application.game.card.CardAction;
+import com.tpps.application.game.card.ServerCard;
 
 /**
  * 
@@ -18,4 +22,76 @@ public class ServerPlayer extends Player {
 		this.deck = deck;
 		// this.id = GameController.getPlayerID();
 	}
+	
+	/**
+	 * calls the static method which executes the actions
+	 * 
+	 * @author ladler - Lukas Adler
+	 */
+	public void doAction(String cardId) {
+		ServerCard serverCard = null;/*this.getDeck().getCard(cardId)*/;
+		ArrayList<CardAction> actionsList = new ArrayList<CardAction>(serverCard
+				.getActions().keySet());
+		
+		// Player player = GameController.getActivePlayer();
+		for (int i = 0; i < actionsList.size(); i++) {
+			switch (actionsList.get(i)) {
+			case ADD_ACTION_TO_PLAYER:
+				// player.addAction(this.actions.get(ADD_ACTION_TO_PLAYER));
+				// sentPackage(player, numbAction);
+				System.out.println("ADD_ACTION_TO_PLAYER: "
+						+ serverCard.getActions().get(
+								CardAction.ADD_ACTION_TO_PLAYER));
+				break;
+			case ADD_PURCHASE:
+				System.out.println("ADD_PURCHASE: "
+						+ serverCard.getActions().get(CardAction.ADD_PURCHASE));
+				break;
+			case ADD_TEMPORARY_MONEY_FOR_TURN:
+				System.out.println("ADD_TEMPORARY_MONEY_FOR_TURN: "
+						+ serverCard.getActions().get(
+								CardAction.ADD_TEMPORARY_MONEY_FOR_TURN));
+				break;
+			case DRAW:
+				System.out.println("DRAW: "
+						+ serverCard.getActions().get(CardAction.DRAW));
+				break;
+			case GAIN:
+				System.out.println("GAIN: "
+						+ serverCard.getActions().get(CardAction.GAIN));
+				break;
+			case DISCARD:
+				System.out.println("DISCARD: "
+						+ serverCard.getActions().get(CardAction.DISCARD));
+				break;
+			case TRASH:
+				System.out.println("TRASH: "
+						+ serverCard.getActions().get(CardAction.TRASH));
+				break;
+			case PUT_BACK:
+				System.out.println("PUT_BACK: "
+						+ serverCard.getActions().get(CardAction.PUT_BACK));
+				break;
+			case REVEAL:
+				System.out.println("REVEAL: "
+						+ serverCard.getActions().get(CardAction.REVEAL));
+				break;
+			case NONE:
+				System.out.println("NONE: "
+						+ serverCard.getActions().get(CardAction.NONE));
+				break;
+			case COUNT_FOR_VICTORY:
+				System.out.println("COUNT_VOR_VICTORY: "
+						+ serverCard.getActions().get(CardAction.COUNT_FOR_VICTORY));
+				break;
+			default:
+				// call
+				break;
+			}
+		}
+	}
+
+	
+	
+	
 }
--- Dominion-8/src/com/tpps/application/game/card/ServerCard.java
+++ Dominion-8/src/com/tpps/application/game/card/ServerCard.java
@@ -24,73 +24,7 @@ public class ServerCard extends Card {
 		super(actions, types, name, cost, _parent);
 	}
 
-	/**
-	 * calls the static method which executes the actions
-	 * 
-	 * @author ladler - Lukas Adler
-	 */
-	public void doAction(String cardId) {
-		ArrayList<CardAction> actionsList = new ArrayList<CardAction>(super
-				.getActions().keySet());
-//		ServerCard = player.getDeck().getCard(cardId)
-		// Player player = GameController.getActivePlayer();
-		for (int i = 0; i < actionsList.size(); i++) {
-			switch (actionsList.get(i)) {
-			case ADD_ACTION_TO_PLAYER:
-				// player.addAction(this.actions.get(ADD_ACTION_TO_PLAYER));
-				// sentPackage(player, numbAction);
-				System.out.println("ADD_ACTION_TO_PLAYER: "
-						+ super.getActions().get(
-								CardAction.ADD_ACTION_TO_PLAYER));
-				break;
-			case ADD_PURCHASE:
-				System.out.println("ADD_PURCHASE: "
-						+ super.getActions().get(CardAction.ADD_PURCHASE));
-				break;
-			case ADD_TEMPORARY_MONEY_FOR_TURN:
-				System.out.println("ADD_TEMPORARY_MONEY_FOR_TURN: "
-						+ super.getActions().get(
-								CardAction.ADD_TEMPORARY_MONEY_FOR_TURN));
-				break;
-			case DRAW:
-				System.out.println("DRAW: "
-						+ super.getActions().get(CardAction.DRAW));
-				break;
-			case GAIN:
-				System.out.println("GAIN: "
-						+ super.getActions().get(CardAction.GAIN));
-				break;
-			case DISCARD:
-				System.out.println("DISCARD: "
-						+ super.getActions().get(CardAction.DISCARD));
-				break;
-			case TRASH:
-				System.out.println("TRASH: "
-						+ super.getActions().get(CardAction.TRASH));
-				break;
-			case PUT_BACK:
-				System.out.println("PUT_BACK: "
-						+ super.getActions().get(CardAction.PUT_BACK));
-				break;
-			case REVEAL:
-				System.out.println("REVEAL: "
-						+ super.getActions().get(CardAction.REVEAL));
-				break;
-			case NONE:
-				System.out.println("NONE: "
-						+ super.getActions().get(CardAction.NONE));
-				break;
-			case COUNT_FOR_VICTORY:
-				System.out.println("COUNT_VOR_VICTORY: "
-						+ super.getActions().get(CardAction.COUNT_FOR_VICTORY));
-				break;
-			default:
-				// call
-				break;
-			}
-		}
-	}
-
+	
 	/**
 	 * @override toString()
 	 */
