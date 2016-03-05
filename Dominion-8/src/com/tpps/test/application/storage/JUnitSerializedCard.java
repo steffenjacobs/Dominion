43901fb1120d4e811dba6890ce688b545627687f
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2016-03-05 23:37:56 +0100

    [DOM8-57] renamed JUnit-Test for SerializedCard-class and added JavaDoc

--- Dominion-8/src/com/tpps/test/application/storage/JUnitSerializedCard.java
+++ Dominion-8/src/com/tpps/test/application/storage/JUnitSerializedCard.java
@@ -20,12 +20,21 @@ import com.tpps.application.game.card.CardAction;
 import com.tpps.application.game.card.CardType;
 import com.tpps.application.storage.SerializedCard;
 
-public class JUnitCardSerializedCard {
-	private static final boolean SHOW_COMAPARE_WINDOW_AFTERWARDS = false;
+/**
+ * JUnit-Test for SerializedCard-Class
+ * 
+ * tests whether a card can be serialized and deserialized via
+ * SerializedCard.getBytes() and new SerializedCard(byte[])
+ * 
+ * @author Steffen Jacobs
+ */
+public class JUnitSerializedCard {
+	private static final boolean DEBUG = false;
 
 	@Test
 	public void test() throws IOException, InterruptedException {
 
+		// create test-card
 		LinkedHashMap<CardAction, Integer> actions = new LinkedHashMap<>();
 		actions.put(CardAction.ADD_ACTION_TO_PLAYER, 2);
 		actions.put(CardAction.DRAW_CARD, 5);
@@ -37,16 +46,30 @@ public class JUnitCardSerializedCard {
 				.read(getClass().getClassLoader().getResourceAsStream("resources/img/gameObjects/testButton.png"));
 
 		SerializedCard sc = new SerializedCard(actions, types, 5, "☢TestCardäöü☢", img);
-		System.out.println(sc.toString());
 
+		// write card to console before serializing
+		if (DEBUG)
+			System.out.println(sc.toString());
+
+		// serialize card
 		byte[] serializedCard = sc.getBytes();
-		SerializedCard sc2 = new SerializedCard(serializedCard);
-		System.out.println(sc2.toString());
 
+		// deserialize card
+		SerializedCard sc2 = new SerializedCard(serializedCard);
+
+		// write card to console after deserializing
+		if (DEBUG)
+			System.out.println(sc2.toString());
+
+		// check if names are equal
 		assertEquals(sc, sc2);
+
+		// check if all other properties (including image) are equal
 		assertTrue(sc.equalsEntirely(sc2));
 
-		if (SHOW_COMAPARE_WINDOW_AFTERWARDS) {
+		// show JFrame so the tester can SEE whether the two images are
+		// identical.
+		if (DEBUG) {
 			JFrame frame = new JFrame();
 			frame.setSize(img.getWidth() * 2 + 22, img.getHeight() + 45);
 			frame.setVisible(false);
