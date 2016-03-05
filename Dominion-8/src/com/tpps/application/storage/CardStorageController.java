df8c8d525e6415b82f9863789583b023e2d8f714
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2016-03-05 23:27:18 +0100

    [DOM8-39] added JavaDoc for CardStorageController

--- Dominion-8/src/com/tpps/application/storage/CardStorageController.java
+++ Dominion-8/src/com/tpps/application/storage/CardStorageController.java
@@ -11,10 +11,19 @@ import java.util.concurrent.ConcurrentHashMap;
 import com.tpps.application.game.card.Card;
 import com.tpps.technicalServices.util.ByteUtil;
 
+/**
+ * This class represents a card-storage
+ * 
+ * @author Steffen Jacobs
+ */
 public final class CardStorageController {
+
 	private static ConcurrentHashMap<String, SerializedCard> storedCards = new ConcurrentHashMap<String, SerializedCard>();
 	private static final String STORAGE_FILE = "cards.bin";
 
+	/**
+	 * loads all cards from file
+	 */
 	public static void loadCards() {
 		try {
 			byte[] bytes = Files.readAllBytes(Paths.get(STORAGE_FILE));
@@ -36,6 +45,9 @@ public final class CardStorageController {
 
 	}
 
+	/**
+	 * saves all stored cards to file
+	 */
 	public static void saveCards() {
 		try {
 			ByteArrayOutputStream bytes = new ByteArrayOutputStream();
@@ -56,27 +68,68 @@ public final class CardStorageController {
 		}
 	}
 
+	/**
+	 * returns a card by the name (if exists) from the storage
+	 * 
+	 * @param name
+	 *            the name of the card
+	 * @return the requested card or null
+	 */
 	public static SerializedCard getCard(String name) {
 		return storedCards.get(name);
 	}
 
+	/**
+	 * adds a card to the storage
+	 * 
+	 * @param card
+	 *            the card to add to the storage. Note: The card will be
+	 *            packaged internally into a different class-type. Note 2: adds
+	 *            the card to the file after calling save().
+	 */
 	public static void addCard(Card card) {
 		storedCards.put(card.getName(), new SerializedCard(card.getActions(), card.getTypes(), card.getCost(),
 				card.getName(), (BufferedImage) card.getImage()));
 	}
 
+	/**
+	 * adds a card to the storage
+	 * 
+	 * @param card
+	 *            the card to add to the storage. Note: This Card-Object will be
+	 *            directly added to the storage. Note 2: Adds the Card to the
+	 *            file after calling save().
+	 */
 	public static void addCard(SerializedCard card) {
 		storedCards.put(card.getName(), card);
 	}
 
+	/**
+	 * removes a card from the storage Note: Removes the card from the file
+	 * after calling save().
+	 * 
+	 * @param card
+	 *            the card to be removed
+	 */
 	public static SerializedCard removeCard(SerializedCard card) {
 		return storedCards.remove(card.getName());
 	}
 
+	/**
+	 * removes a card from the storage. Note: Removes the card from the file
+	 * after calling save().
+	 * 
+	 * @param card
+	 *            the name of the card to be removed
+	 */
 	public static SerializedCard removeCard(String cardName) {
 		return storedCards.remove(cardName);
 	}
 
+	/**
+	 * removes all cards from the storage. Note: Removes the cards from the file
+	 * after calling save().
+	 */
 	public static void clearCards() {
 		storedCards.clear();
 	}
