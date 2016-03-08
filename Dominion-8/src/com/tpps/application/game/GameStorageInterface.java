30ae5cdb3f5c237990f67e29bbc1caf80c13a5b9
Lukas Karl Adler <ladler@mail.uni-mannheim.de>
2016-03-08 01:53:44 +0100

    [DOM8-52] drawBoard drawHandCards

--- Dominion-8/src/com/tpps/application/game/GameStorageInterface.java
+++ Dominion-8/src/com/tpps/application/game/GameStorageInterface.java
@@ -20,25 +20,45 @@ public class GameStorageInterface {
 		this.gameWindow = gameWindow;
 	}
 	
+	public void loadHandCardsAndPassToGameWindow(LinkedList<String> handCardIds){
+		LinkedHashMap<String, SerializedCard> serializedCardWithId = loadCards(handCardIds);
+			 this.gameWindow.handCards(serializedCardWithId);
+		}
+
+	private LinkedHashMap<String, SerializedCard> loadCards(LinkedList<String> handCardIds) {
+		CardStorageController cs =  new CardStorageController();
+		LinkedHashMap<String, SerializedCard> serializedCardWithId = new LinkedHashMap<String, SerializedCard>();
+		cs.loadCards();
+		
+		 for (Iterator<String> iterator = handCardIds.iterator(); iterator.hasNext();) {
+				String handCardId = (String) iterator.next();
+				SerializedCard serializedCard = cs.getCard(handCardId.substring(0, handCardId.length() - 1));
+			
+				
+				serializedCard = new SerializedCard(serializedCard.getActions(), serializedCard.getTypes(), 
+						serializedCard.getCost(), serializedCard.getName(), serializedCard.getImage());
+				serializedCardWithId.put(handCardId, serializedCard);			
+			}
+		return serializedCardWithId;
+	}
+
+	
 	public void loadActionCardsAndPassToGameWindow(LinkedList<String> actionCardIds){
 		CardStorageController cs =  new CardStorageController();
 		LinkedHashMap<String, SerializedCard> serializedCardWithId = new LinkedHashMap<String, SerializedCard>();
-		for (Iterator<String> iterator = actionCardIds.iterator(); iterator.hasNext();) {
-			String string = (String) iterator.next();
-			System.out.println(string);
-			
-		}
+		
 		 cs.loadCards();
+		 cs.listCards();
 		 
 		 for (Iterator<String> iterator = actionCardIds.iterator(); iterator.hasNext();) {
 			String actionCardId = (String) iterator.next();
 			SerializedCard serializedCard = cs.getCard(actionCardId.substring(0, actionCardId.length() - 1));
-			System.out.println(serializedCard);
-			System.out.println("actions: " +serializedCard.getActions());
-			System.out.println("types: " +serializedCard.getTypes());
-			System.out.println("costs: " + serializedCard.getCost());
-			System.out.println("name: " + serializedCard.getName());
-			System.out.println(serializedCard.getImage());
+//			System.out.println(serializedCard);
+//			System.out.println("actions: " +serializedCard.getActions());
+//			System.out.println("types: " +serializedCard.getTypes());
+//			System.out.println("costs: " + serializedCard.getCost());
+//			System.out.println("name: " + serializedCard.getName());
+//			
 			serializedCard = new SerializedCard(serializedCard.getActions(), serializedCard.getTypes(), 
 					serializedCard.getCost(), serializedCard.getName(), serializedCard.getImage());
 			serializedCardWithId.put(actionCardId, serializedCard);			
@@ -48,7 +68,7 @@ public class GameStorageInterface {
 	
 	public static void main(String[] args) {
 		try {
-			new GameStorageInterface(new GameWindow()).loadActionCardsAndPassToGameWindow(CollectionsUtil.linkedList(new String[]{"Cellar1"}));
+			new GameStorageInterface(new GameWindow()).loadActionCardsAndPassToGameWindow(CollectionsUtil.linkedList(new String[]{"Cellar2"}));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
