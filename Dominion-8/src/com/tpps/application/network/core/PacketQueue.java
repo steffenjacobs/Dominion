f9b04e62aaa87d1bf85c7b8e20972c7a99114b10
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2016-03-09 19:28:35 +0100

    [DOM8-24] added javaDoc for PacketQueue

--- Dominion-8/src/com/tpps/application/network/core/PacketQueue.java
+++ Dominion-8/src/com/tpps/application/network/core/PacketQueue.java
@@ -3,18 +3,37 @@ package com.tpps.application.network.core;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Semaphore;
 
+/**
+ * This class represents a packet-queue
+ * 
+ * @author Steffen Jacobs
+ */
 public class PacketQueue<E> extends ConcurrentLinkedQueue<E> {
 	private static final long serialVersionUID = 9133220497162299860L;
 
 	private Semaphore lock = new Semaphore(1);
 
+	/**
+	 * adds the element to the tail of the queue and notifies the consumer (if
+	 * not already consuming)
+	 * 
+	 * @param elem
+	 *            element to add to the queue
+	 * 
+	 * @return true as specified by Queue.offer(E))
+	 */
 	@Override
-	public boolean offer(E e) {
-		super.offer(e);
+	public boolean offer(E elem) {
+		super.offer(elem);
 		lock.release();
 		return true;
 	}
 
+	/**
+	 * removes the head of the queue, blocks if queue is empty
+	 * 
+	 * @return the head of the queue
+	 */
 	@Override
 	public E poll() {
 		try {
@@ -25,6 +44,11 @@ public class PacketQueue<E> extends ConcurrentLinkedQueue<E> {
 		return super.poll();
 	}
 
+	/**
+	 * returns the packet at the head of the queue, blocks if queue is empty
+	 * 
+	 * @return the head of the queue
+	 */
 	@Override
 	public E peek() {
 		try {
@@ -35,6 +59,12 @@ public class PacketQueue<E> extends ConcurrentLinkedQueue<E> {
 		return super.peek();
 	}
 
+	/**
+	 * adds a packet to the queue and notifies the consumer (if not already
+	 * consuming)
+	 * 
+	 * @return false as specified by Queue.offer(E))
+	 */
 	@Override
 	public boolean add(E e) {
 		super.add(e);
