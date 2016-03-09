f204804937b44ec718cc090c11107d8f5b423220
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2016-03-09 19:19:43 +0100

    [DOM8-30] removed MultiMap, added getLongBytes to ByteUtil

--- Dominion-8/src/com/tpps/technicalServices/util/ByteUtil.java
+++ Dominion-8/src/com/tpps/technicalServices/util/ByteUtil.java
@@ -83,4 +83,16 @@ public class ByteUtil {
 		return new byte[] { (byte) (value >>> 24), (byte) (value >>> 16), (byte) (value >>> 8), (byte) value };
 	}
 
+	/**
+	 * converts a long to a byte-array
+	 * 
+	 * @param value
+	 *            long to convert
+	 * @return converted long
+	 */
+	public static final byte[] longToByteArray(long value) {
+		return new byte[] { (byte) (value >>> 56), (byte) (value >>> 48), (byte) (value >>> 40), (byte) (value >>> 32),
+				(byte) (value >>> 24), (byte) (value >>> 16), (byte) (value >>> 8), (byte) value };
+	}
+
 }
