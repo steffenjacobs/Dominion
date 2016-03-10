c570959e10f9256c26b47fbd6ef9f6417914f31b
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2016-03-10 20:29:23 +0100

    [DOM8-69] added move-animation & updated visual-tester for it

--- Dominion-8/src/com/tpps/test/application/ui/VisualTester.java
+++ Dominion-8/src/com/tpps/test/application/ui/VisualTester.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.concurrent.Callable;
 
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
@@ -16,7 +17,8 @@ import com.tpps.technicalServices.util.CollectionsUtil;
 import com.tpps.technicalServices.util.GraphicsUtil;
 import com.tpps.ui.GameObject;
 import com.tpps.ui.GraphicFramework;
-import com.tpps.ui.RelativeGeom2D;
+import com.tpps.ui.animations.FadeOutAnimation;
+import com.tpps.ui.animations.MoveAnimation;
 import com.tpps.ui.components.GFButton;
 
 public class VisualTester extends JFrame {
@@ -27,8 +29,8 @@ public class VisualTester extends JFrame {
 	public static VisualTester getInstance() {
 		return instance;
 	}
-	
-	public static VisualTester setInstance(VisualTester gw){
+
+	public static VisualTester setInstance(VisualTester gw) {
 		instance = gw;
 		return gw;
 	}
@@ -47,12 +49,12 @@ public class VisualTester extends JFrame {
 	 */
 	public VisualTester() throws IOException {
 		instance = this;
-//		JFrame frame = new JFrame();
-//		frame.setSize(100, 100);
-//		GraphicFramework gf = new GraphicFramework(frame);
-//		gf.setSize(100, 100);
-//		gf.addComponent(new Card(null, null, "Lachs", 100,gf));
-//		frame.add(gf);
+		// JFrame frame = new JFrame();
+		// frame.setSize(100, 100);
+		// GraphicFramework gf = new GraphicFramework(frame);
+		// gf.setSize(100, 100);
+		// gf.addComponent(new Card(null, null, "Lachs", 100,gf));
+		// frame.add(gf);
 		final int WIDTH = 1280, HEIGHT = 720;
 
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -68,32 +70,50 @@ public class VisualTester extends JFrame {
 
 		framework.addComponent(new TestButton(.3, .3, .4, .4, WIDTH, HEIGHT, 6, im, framework, "first"));
 		framework.addComponent(new TestButton(.2, .2, .4, .4, WIDTH, HEIGHT, 4, im, framework, "second"));
-		
+
 		GFButton gfb = new TestButton(.1, .1, .4, .4, WIDTH, HEIGHT, 5, im, framework, "third");
-		
+
 		this.revalidate();
 		framework.addComponent(gfb);
 		Card.resetClassID();
-		framework.addComponent(new Card(CollectionsUtil.linkedHashMapAction(CollectionsUtil.linkedList(CardAction.ADD_ACTION_TO_PLAYER), CollectionsUtil.linkedList("2")), 
-				CollectionsUtil.linkedList(CardType.ACTION), "Copper", 4, 0.8, 0.9, 0.9, 0.9, 1, ImageIO.read(ClassLoader
-				.getSystemResource("resources/img/mainMenu/Dominion.jpg")), this.framework));
-		
+		framework.addComponent(new Card(
+				CollectionsUtil.linkedHashMapAction(CollectionsUtil.linkedList(CardAction.ADD_ACTION_TO_PLAYER),
+						CollectionsUtil.linkedList("2")),
+				CollectionsUtil.linkedList(CardType.ACTION), "Copper", 4, 0.8, 0.9, 0.9, 0.9, 1,
+				ImageIO.read(ClassLoader.getSystemResource("resources/img/mainMenu/Dominion.jpg")), this.framework));
 
-		new Thread(() -> {
-			try {
-				Thread.sleep(2000);
-				framework.moveObject(gfb, new RelativeGeom2D(.4, .4));
-				Thread.sleep(2000);
-				gfb.updateImage(GraphicsUtil.setAlpha(gfb.getOriginalImage(), .5f));
-				Thread.sleep(5000);
-				framework.removeComponent(gfb);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}).start();
+		// new Thread(() -> {
+		// try {
+		// Thread.sleep(2000);
+		// framework.moveObject(gfb, new RelativeGeom2D(.4, .4));
+		// Thread.sleep(2000);
+		// gfb.updateImage(GraphicsUtil.setAlpha(gfb.getOriginalImage(), .5f));
+		// Thread.sleep(5000);
+		// framework.removeComponent(gfb);
+		// } catch (Exception e) {
+		// e.printStackTrace();
+		// }
+		// }).start();
 
 		this.revalidate();
 		this.repaint();
+
+		FadeOutAnimation anim = new FadeOutAnimation(gfb, 1000, new Callable<Void>() {
+			@Override
+			public Void call() throws Exception {
+				System.out.println("finished here!");
+				return null;
+			}
+		});
+		MoveAnimation anim2 = new MoveAnimation(framework, gfb, 2000, new Callable<Void>() {
+			@Override
+			public Void call() throws Exception {
+				System.out.println("finished here!");
+				return null;
+			}
+		}, .7, .7);
+		anim.play();
+		anim2.play();
 	}
 
 	private class TestButton extends GFButton {
