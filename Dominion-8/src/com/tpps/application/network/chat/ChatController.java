c0fa2d7a5d54c18d5d8d0008337e7a24424d335b
Johannes Nicolas Huhn <jhuhn@mail.uni-mannheim.de>
2016-03-10 02:30:11 +0100

    [DOM8-47] created model for chatcontroller

---
+++ Dominion-8/src/com/tpps/application/network/chat/ChatController.java
@@ -0,0 +1,26 @@
+package com.tpps.application.network.chat;
+
+import java.util.ArrayList;
+
+public class ChatController {
+
+	public ChatController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void createChatroom(ArrayList<String> members){
+		
+	}
+	
+	public void createChatroom(String user1, String user2, String user3, String user4){
+		
+	}
+	
+	public void deleteChatroom(int id){
+		
+	}
+	
+	public void deleteChatroom(String onemember){
+		
+	}
+}
--- Dominion-8/src/com/tpps/application/network/chat/ChatUtils.java
+++
@@ -1,7 +0,0 @@
-package com.tpps.application.network.chat;
-
-public class ChatUtils {
-
-	private final static String servercommand1 = "help";
-	private final static String servercommand2 = "cmd";
-}
