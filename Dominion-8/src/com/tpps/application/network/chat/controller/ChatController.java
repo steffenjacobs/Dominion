050eb32e1845aaff6d706f31e5c6f49aa76cfb13
Johannes Nicolas Huhn <jhuhn@mail.uni-mannheim.de>
2016-03-11 08:25:58 +0100

    [DOM8-47] implemented a chatcontroller, which is usefull to create/delete chatrooms

--- Dominion-8/src/com/tpps/application/network/chat/controller/ChatController.java
+++ Dominion-8/src/com/tpps/application/network/chat/controller/ChatController.java
@@ -1,26 +1,71 @@
 package com.tpps.application.network.chat.controller;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class ChatController {
+import com.tpps.application.network.chat.packets.PacketChatController;
+import com.tpps.application.network.core.Client;
+import com.tpps.application.network.core.PacketHandler;
+import com.tpps.application.network.core.packet.Packet;
+
+public class ChatController extends PacketHandler{
 	
-	public ChatController(String user) {
-
+	private Client chatclient;
+	
+	public ChatController() {
+		try {
+			chatclient = new Client(new InetSocketAddress("127.0.0.1", 1340), this, false);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void createChatroom(ArrayList<String> members){
-		
-	}
-	
-	public void createChatroom(String user1, String user2, String user3, String user4){
-		
-	}
-	
-	public void deleteChatroom(int id){
-		
+		PacketChatController packet = new PacketChatController("createChatroom", members);
+		try {
+			this.chatclient.sendMessage(packet);
+		} catch (IOException e) {		
+			e.printStackTrace();
+		}
 	}
 	
 	public void deleteChatroom(String onemember){
-		
+		PacketChatController packet = new PacketChatController("deleteChatroom", onemember);
+		try {
+			this.chatclient.sendMessage(packet);
+		} catch (IOException e) {		
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void handleReceivedPacket(int port, Packet packet) { }
+	
+	public static void main(String[] args) {
+		ChatController troller = new ChatController();
+		System.out.println("I am: the chatcontroller");
+		Scanner scanInput = new Scanner(System.in);
+		String line = null;
+		while(true){
+			line = scanInput.nextLine();
+			if(line.startsWith("create")){
+				String[] split = line.split("\\s+");
+				ArrayList<String> list = new ArrayList<String>();
+				list.add(split[1]);
+				list.add(split[2]);
+				list.add(split[3]);
+				list.add(split[4]);
+				troller.createChatroom(list);
+				System.out.println("create chatroom");
+			}else if(line.startsWith("del")){
+				String[] split = line.split("\\s+");
+				troller.deleteChatroom(split[1]);
+				System.out.println("delete chatroom");
+			}else{
+				System.out.println("bad command");
+			}
+		}
 	}
 }
