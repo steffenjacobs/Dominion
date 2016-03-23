3f2f5218df1a61743b9080f1a9c844cf45a5faef
Johannes Nicolas Huhn <jhuhn@mail.uni-mannheim.de>
2016-03-23 20:05:07 +0100

    [DOM8-72] statisticsboard looks now better

--- Dominion-8/src/com/tpps/ui/statisticsscreen/StatisticsBoard.java
+++ Dominion-8/src/com/tpps/ui/statisticsscreen/StatisticsBoard.java
@@ -2,16 +2,20 @@ package com.tpps.ui.statisticsscreen;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.JTableHeader;
 
 import com.tpps.technicalServices.util.GraphicsUtil;
 
@@ -21,10 +25,13 @@ public class StatisticsBoard extends JPanel{
 	private static final int VERTICAL_STRUT = 30;
 	private static final int HORIZONTAL_STRUT = 50;
 	private static final float BLACK_TRANSPARENCY = 0.6F;
+	private static final int CELL_ROW_HEIGHT = 50;
 	private BufferedImage blackBeauty;
+	private final Font font = new Font("Calibri", Font.PLAIN, 15);
+	private final Font headFont = new Font("Arial Black", Font.BOLD, 14);
 	
 	Object rowData[][] = { { "Row1-Column1", "Row1-Column2", "Row1-Column3"}, { "Row2-Column1", "Row2-Column2", "Row2-Column3"} };
-	Object columnNames[] = { "Column One", "Column Two", "Column Three"};
+	Object columnNames[] = { "nickname", "wins", "losses", "w/l ratio", "total matches" , "rank", "playtime"};
 	
 	public StatisticsBoard() {
 		rowData = this.initRowData();
@@ -40,7 +47,7 @@ public class StatisticsBoard extends JPanel{
 	}
 	
 	private String[][] initRowData(){
-		String[][] data = new String[1000][3];
+		String[][] data = new String[1000][7];
 		for (int i = 0; i < data.length; i++) {
 			for (int j = 0; j < data[i].length; j++) {
 				data[i][j] = "Row_" + i + " Column_" + j ;
@@ -67,9 +74,11 @@ public class StatisticsBoard extends JPanel{
 		};
 		table.setOpaque(false);
 		((DefaultTableCellRenderer)table.getDefaultRenderer(Object.class)).setOpaque(false);
-		table.setForeground(Color.WHITE);
+		table.setForeground(Color.WHITE);		
 		table.setFocusable(false);
 		table.setEnabled(false);
+		table.setFont(font);
+		table.setRowHeight(CELL_ROW_HEIGHT);		
 		
 		JScrollPane pane = new JScrollPane(table){
 
@@ -85,15 +94,15 @@ public class StatisticsBoard extends JPanel{
 		pane.setVisible(true);
 		pane.getVerticalScrollBar().setOpaque(false);
 		pane.getViewport().setOpaque(false);
+		pane.setBorder(BorderFactory.createEmptyBorder());
 		
-//		JTableHeader header = new JTableHeader(){
-//			@Override
-//			public void paint(Graphics g) {
-//				g.drawImage(blackBeauty, 0, 0, null);
-//				super.paint(g);
-//			}
-//		};
-//		header.setForeground(Color.WHITE);
+		JTableHeader header = table.getTableHeader();
+		header.setBackground(Color.BLACK);
+		header.setForeground(Color.WHITE);
+		header.setEnabled(true);
+		header.setFont(headFont);
+		header.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		header.setReorderingAllowed(false);
 		
 		return pane;
 	}
