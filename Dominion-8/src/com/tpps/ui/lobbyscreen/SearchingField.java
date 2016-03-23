52a621082c1c594342d477801ec8fef9164af148
Johannes Nicolas Huhn <jhuhn@mail.uni-mannheim.de>
2016-03-23 23:55:01 +0100

    [DOM8-78] javadoc for the lobbyscreen started

--- Dominion-8/src/com/tpps/ui/lobbyscreen/SearchingField.java
+++ Dominion-8/src/com/tpps/ui/lobbyscreen/SearchingField.java
@@ -17,8 +17,12 @@ public class SearchingField extends JTextField implements Runnable{
 	private static final long serialVersionUID = 1L;
 	private BufferedImage blackBeauty;
 	private final Font font = new Font("Calibri", Font.PLAIN, 20);
+	private static final float BLACK_TRANSPARENCY = 0.6F;
 	private boolean playerFlag;
 	
+	/**
+	 * initializes the object, load image
+	 */
 	public SearchingField() {
 		playerFlag = false;
 		this.setFont(font);
@@ -31,22 +35,34 @@ public class SearchingField extends JTextField implements Runnable{
 		
 		try {
 			this.blackBeauty = ImageIO.read(ClassLoader.getSystemResource("resources/img/lobbyScreen/blackbeauty.png"));
-			blackBeauty = (BufferedImage) GraphicsUtil.setAlpha(blackBeauty, 0.6F);
+			blackBeauty = (BufferedImage) GraphicsUtil.setAlpha(blackBeauty, BLACK_TRANSPARENCY);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * overrides the paint method for semitransparend causes
+	 */
 	@Override
 	public void paint(Graphics g) {							
 		g.drawImage(blackBeauty, 0, 0, null);
 		super.paint(g);
 	}
 	
+	/**
+	 * starts the thread, searching procedure on UI starts
+	 */
 	public void start(){
 		new Thread(this).start();
 	}
 
+	/**
+	 * executes the searching procedure. possible states:
+	 * 	-> Loading .
+	 *  -> Loading ..
+	 *  -> Loading ...
+	 */
 	@Override
 	public void run() {
 		int points = 1;
@@ -69,10 +85,19 @@ public class SearchingField extends JTextField implements Runnable{
 		}
 	}
 
+	/**
+	 * @return a boolean which shows if a player already joined
+	 * 	true, a player already joined the lobby
+	 *  false, no player joined for this searchingfield 
+	 */
 	public boolean isPlayerFlag() {
 		return playerFlag;
 	}
 
+	/**
+	 * ends the searching procedure on the UI and sets the playername who joined
+	 * @param playername a String representation of the playername who joined the lobby
+	 */
 	public void setPlayer(String playername) {
 		this.playerFlag = true;
 		this.setText(playername);
