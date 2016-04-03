50e05db9dc627f935f8f9c1890895a0258e812b7
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2016-04-03 14:59:29 +0200

    [DOM8-67] updated AutoCreatingProperties & added JavaDoc

--- Dominion-8/src/com/tpps/technicalServices/util/AutoCreatingProperties.java
+++ Dominion-8/src/com/tpps/technicalServices/util/AutoCreatingProperties.java
@@ -4,8 +4,18 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
 import java.util.Properties;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * represents an optimized version of the normal java.util.Properties,
+ * auto-saves everytime a porperty is set/get
+ * 
+ * @author Steffen Jacobs
+ */
 public class AutoCreatingProperties extends Properties {
 	private static final long serialVersionUID = -8882987098297632096L;
 	private File f;
@@ -14,8 +24,16 @@ public class AutoCreatingProperties extends Properties {
 		super();
 	}
 
-	public void load(File file) {
+	public void load(InputStream is) {
+		throw new NotImplementedException();
+	}
 
+	public void load(Reader reader) {
+		throw new NotImplementedException();
+	}
+
+	/** loads the config from a file */
+	public void load(File file) {
 		try {
 			if (!file.exists()) {
 				file.createNewFile();
@@ -28,6 +46,18 @@ public class AutoCreatingProperties extends Properties {
 		}
 	}
 
+	/** sets or creates a property and saves afterwards */
+	public Object setProperty(String key, String value) {
+		Object obj = super.setProperty(key, value);
+		try {
+			super.store(new FileOutputStream(f), null);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return obj;
+	}
+
+	/** @return the key to a property-name & saves afterwards */
 	public String getProperty(String name, String def) {
 		if (super.containsKey(name)) {
 			return super.getProperty(name);
