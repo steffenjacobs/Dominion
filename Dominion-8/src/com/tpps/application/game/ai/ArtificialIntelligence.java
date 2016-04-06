d5d94e5a868b83e1b059e4a7fe4efac0a7a5a2c4
Nicolas Christian Wipfler <nwipfler@mail.uni-mannheim.de>
2016-04-06 23:43:42 +0200

    [DOM8-61] Added LinkedListMultimap

--- Dominion-8/src/com/tpps/application/game/ai/ArtificialIntelligence.java
+++ Dominion-8/src/com/tpps/application/game/ai/ArtificialIntelligence.java
@@ -2,6 +2,8 @@ package com.tpps.application.game.ai;
 
 import java.util.LinkedList;
 
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.ListMultimap;
 import com.tpps.application.game.Player;
 import com.tpps.application.game.card.Card;
 import com.tpps.technicalServices.network.game.GameServer;
@@ -11,12 +13,56 @@ public class ArtificialIntelligence {
 
 	private Player player;
 	private LinkedList<Card> blacklist;
-	
-	//board anschauen, wenn angriffskarten gekauft werden dann defensiv kaufen
-	//wenn es nix bringt, mehr karten zu ziehen, ggf. aktionskarten nicht spielen
+	private ListMultimap<String, Card> aiActions;
 
-	public ArtificialIntelligence(int clientID, int port, LinkedList<Card> initCards) {
+	// board anschauen, wenn angriffskarten gekauft werden dann defensiv kaufen
+	// wenn es nix bringt, mehr karten zu ziehen, ggf. aktionskarten nicht
+	// spielen
+	// LinkedListMultimap mit "buy" oder "play" und karte als Spielplan aufbauen
+
+	public ArtificialIntelligence(int clientID, int port,
+			LinkedList<Card> initCards) {
 		this.player = new Player(clientID, port, initCards);
+		this.blacklist = new LinkedList<Card>();
+		this.aiActions = LinkedListMultimap.create();
+	}
+
+	/**
+	 * @return the blacklist
+	 */
+	public LinkedList<Card> getBlacklist() {
+		return blacklist;
+	}
+
+	/**
+	 * @param blacklist
+	 *            the blacklist to set
+	 */
+	public void setBlacklist(LinkedList<Card> blacklist) {
+		this.blacklist = blacklist;
+	}
+
+	/**
+	 * @return the aiActions
+	 */
+	public ListMultimap<String, Card> getAiActions() {
+		return aiActions;
+	}
+
+	/**
+	 * @param aiActions
+	 *            the aiActions to set
+	 */
+	public void setAiActions(ListMultimap<String, Card> aiActions) {
+		this.aiActions = aiActions;
+	}
+
+	/**
+	 * @param player
+	 *            the player to set
+	 */
+	public void setPlayer(Player player) {
+		this.player = player;
 	}
 
 	public Player getPlayer() {
@@ -24,9 +70,9 @@ public class ArtificialIntelligence {
 	}
 
 	public void executeTurn() {
-//		checkMyTurn();
+		myTurn();
 
-		LinkedList<Card> cardHand = this.player.getDeck().getCardHand();
+		// LinkedList<Card> cardHand = this.player.getDeck().getCardHand();
 
 		playTreasures();
 
@@ -34,7 +80,8 @@ public class ArtificialIntelligence {
 	}
 
 	private boolean myTurn() {
-		return GameServer.getInstance().getGameController().getActivePlayer().equals(this.player);
+		return GameServer.getInstance().getGameController().getActivePlayer()
+				.equals(this.player);
 	}
 
 	private void endTurn() {
@@ -43,5 +90,13 @@ public class ArtificialIntelligence {
 
 	private void playTreasures() {
 		GameWindow.playTreasures.onMouseClick();
+	}
+
+	public static void main(String[] args) {
+		ListMultimap<String, Integer> map = LinkedListMultimap.create();
+		map.put("a", 2);
+		map.put("b", 3);
+		map.put("a", 3);
+		map.put("c", 2);
 	}
 }
