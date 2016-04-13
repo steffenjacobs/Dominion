2af267f9daf2b91b654c1be20e2398b9b3a09cdb
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2016-04-13 15:47:12 +0200

    [DOM8-73] added javaDoc for PacketMatchmakingAnswer

--- Dominion-8/src/com/tpps/technicalServices/network/matchmaking/packets/PacketMatchmakingAnswer.java
+++ Dominion-8/src/com/tpps/technicalServices/network/matchmaking/packets/PacketMatchmakingAnswer.java
@@ -3,6 +3,12 @@ package com.tpps.technicalServices.network.matchmaking.packets;
 import com.tpps.technicalServices.network.core.packet.Packet;
 import com.tpps.technicalServices.network.core.packet.PacketType;
 
+/**
+ * sent from the MatchmakingServer to a searching client, answer to the
+ * PacketMatchmakingRequest from the client
+ * 
+ * @author Steffen Jacobs
+ */
 public class PacketMatchmakingAnswer extends Packet {
 	private static final long serialVersionUID = 4633962878878433551L;
 
@@ -13,22 +19,34 @@ public class PacketMatchmakingAnswer extends Packet {
 	 */
 	private final int answerCode;
 
+	/**
+	 * constructor with the request and the answer
+	 * 
+	 * @param req
+	 *            request packet received from the client
+	 * @param answerCode
+	 *            0: Error, bad session; 1: Success
+	 */
 	public PacketMatchmakingAnswer(PacketMatchmakingRequest req, int answerCode) {
 		super(PacketType.MATCHMAKING_ANSWER);
 		this.answerCode = answerCode;
 		this.request = req;
 	}
 
+	/** @return a representation of the packet as a String */
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.getClass().getSimpleName() + ": Request:" + this.getRequest() + " - AC: " + this.getAnswerCode();
 	}
 
+	/**
+	 * @return the answerCode of the request: 0: Error, bad session; 1: Success
+	 */
 	public int getAnswerCode() {
 		return answerCode;
 	}
 
+	/** @return the request sent to the server */
 	public PacketMatchmakingRequest getRequest() {
 		return request;
 	}
