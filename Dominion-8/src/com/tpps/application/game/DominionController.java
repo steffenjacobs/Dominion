d938fcaad099bd93caff2517d3d1972664ef8eab
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2016-04-13 21:42:11 +0200

    [DOM8-73] reenabled standard-login

--- Dominion-8/src/com/tpps/application/game/DominionController.java
+++ Dominion-8/src/com/tpps/application/game/DominionController.java
@@ -75,7 +75,7 @@ public final class DominionController {
 	public DominionController() { }
 
 	private void init() {
-		boolean login = false;
+		boolean login = true;
 		if(login){
 			storageController = new CardStorageController();
 			mainFrame = new MainFrame();
