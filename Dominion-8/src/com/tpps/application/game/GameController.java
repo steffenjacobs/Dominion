f40a54e2aaffb0931733db42d34ae67e6c51f02d
Lukas Karl Adler <ladler@mail.uni-mannheim.de>
2016-04-13 19:01:02 +0200

    [DOM8-52] comment

--- Dominion-8/src/com/tpps/application/game/GameController.java
+++ Dominion-8/src/com/tpps/application/game/GameController.java
@@ -117,7 +117,7 @@ public class GameController {
 	}
 
 	/**
-	 * determines the next ative player
+	 * determines the next active player
 	 */
 	public void setNextActivePlayer() {
 		Player activePlayer = this.getActivePlayer();
