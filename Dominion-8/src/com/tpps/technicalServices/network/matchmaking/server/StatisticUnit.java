6e1ceaf3048a4caa77c360537284d6aa7cd527a5
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2016-04-13 15:51:42 +0200

    [DOM8-73] added javaDoc for StatisticsUnit

--- Dominion-8/src/com/tpps/technicalServices/network/matchmaking/server/StatisticUnit.java
+++ Dominion-8/src/com/tpps/technicalServices/network/matchmaking/server/StatisticUnit.java
@@ -1,20 +1,34 @@
 package com.tpps.technicalServices.network.matchmaking.server;
 
+/**
+ * represents a Container-Object for all stats
+ * 
+ * @author Steffen Jacobs
+ */
 public class StatisticUnit {
 	private Object obj;
 
+	/**
+	 * initializes the container with a stat
+	 * 
+	 * @param stat
+	 *            the statistics
+	 */
 	public StatisticUnit(Object stat) {
 		this.obj = stat;
 	}
 
+	/** @return container-interpretation as an integer */
 	public int asInteger() {
 		return (int) obj;
 	}
 
+	/** @return container-interpretation as an arry of longs */
 	public long[] asLongArray() {
 		return (long[]) obj;
 	}
 
+	/** @return container-interpretation as an arry of booleans */
 	public boolean[] asBooleanArray() {
 		return (boolean[]) obj;
 	}
