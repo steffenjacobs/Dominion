aa07d5aec7939a7ccce9df3244a133a0c2fa4163
Lukas Karl Adler <ladler@mail.uni-mannheim.de>
2016-05-04 15:40:30 +0200

    [DOM8-13] test

--- Dominion-8/src/com/tpps/technicalServices/network/game/GameServerNetworkListener.java
+++ Dominion-8/src/com/tpps/technicalServices/network/game/GameServerNetworkListener.java
@@ -34,12 +34,7 @@ public class GameServerNetworkListener implements NetworkListener {
 
 	@Override
 	public void onClientDisconnect(int port) {
-		System.out.println("client disconnected");
-		try {
-			this.gameServer.broadcastMessage(new PacketBroadcastLog(MsgType.INFO, "player disconnected."));
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
+		
 		LinkedList<Player> players = this.gameServer.getGameController().getPlayers();
 		for (Iterator<Player> iterator = players.iterator(); iterator.hasNext();) {
 			Player player = (Player) iterator.next();
@@ -48,58 +43,78 @@ public class GameServerNetworkListener implements NetworkListener {
 //					if (player.getSessionID().equals(UUID.fromString("00000000-0000-0000-0000-000000000000"))) {
 //						return;
 //					}
+					
+					
+					
 					if (!this.gameServer.getDisconnectedUser().contains(player)) {
 						this.gameServer.getDisconnectedUser().add(player);
+						System.out.println("client disconnected");
+						try {
+							this.gameServer.broadcastMessage(new PacketBroadcastLog(MsgType.INFO, "player disconnected."));
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
+						
+						try {
+							this.gameServer.broadcastMessage(new PacketDisable("user disconnected"));
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						
+						new Thread(() -> {
+							System.out.println("observer thread started");		
+							
+							try {
+								Thread.sleep(10000);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+							if (this.gameServer.getDisconnectedUser().size() >= 3) {
+								System.out.println("end game because three users are disconnected");
+								Client client;
+								try {
+									client = new Client(
+											new InetSocketAddress(Addresses.getLocalHost(), MatchmakingServer.getStandardPort()),
+											new PacketHandler() {
+
+												@Override
+												public void handleReceivedPacket(int port, Packet packet) {
+
+												}
+											}, false);
+									client.sendMessage(new PacketGameEnd(this.gameServer.getGameController().getPlayerNames(),
+											this.gameServer.getGameController().getWinningPlayer().getPlayerName(), gameServer.getPort()));
+									this.gameServer.getGameController().endGame();
+								} catch (IOException e) {
+									e.printStackTrace();
+								}
+							}else if (this.gameServer.getDisconnectedUser().size() >= 1) {
+								Player player1 = this.gameServer.getGameController().getPlayerByPort(port);
+								this.gameServer.getGameController().getPlayers().remove(player1);
+								this.gameServer.getDisconnectedUser().remove(player1);
+								this.gameServer.getGameController().setActivePlayer(this.gameServer.getGameController().getRandomPlayer());
+								try {
+									this.gameServer.broadcastMessage(new PacketEnableDisable(this.gameServer.getGameController().getActivePlayer().getClientID(),
+											this.gameServer.getGameController().getActivePlayerName(), false));
+									this.gameServer.broadcastMessage(new PacketBroadcastLog(this.gameServer.getGameController().getPlayers().size() + " players left."));
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							}
+						}).start();
+
+						
+						
+						break;
 					}
+					
+										
+					
 				}
 			}		
-		try {
-			this.gameServer.broadcastMessage(new PacketDisable("user disconnected"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		
 		
-		new Thread(() -> {
-			System.out.println("observer thread started");		
-			
-			try {
-				Thread.sleep(10000);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			if (this.gameServer.getDisconnectedUser().size() >= 3) {
-				System.out.println("end game because three users are disconnected");
-				Client client;
-				try {
-					client = new Client(
-							new InetSocketAddress(Addresses.getLocalHost(), MatchmakingServer.getStandardPort()),
-							new PacketHandler() {
+		
 
-								@Override
-								public void handleReceivedPacket(int port, Packet packet) {
-
-								}
-							}, false);
-					client.sendMessage(new PacketGameEnd(this.gameServer.getGameController().getPlayerNames(),
-							this.gameServer.getGameController().getWinningPlayer().getPlayerName(), gameServer.getPort()));
-					this.gameServer.getGameController().endGame();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}else if (this.gameServer.getDisconnectedUser().size() >= 1) {
-				Player player = this.gameServer.getGameController().getPlayerByPort(port);
-				this.gameServer.getGameController().getPlayers().remove(player);
-				this.gameServer.getDisconnectedUser().remove(player);
-				this.gameServer.getGameController().setActivePlayer(this.gameServer.getGameController().getRandomPlayer());
-				try {
-					this.gameServer.broadcastMessage(new PacketEnableDisable(this.gameServer.getGameController().getActivePlayer().getClientID(),
-							this.gameServer.getGameController().getActivePlayerName(), false));
-					this.gameServer.broadcastMessage(new PacketBroadcastLog(this.gameServer.getGameController().getPlayers().size() + " players left."));
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
 	}
 }
